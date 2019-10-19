281c7c4d4d183f61beb10ed74b17dcea195d069f
“Karan <ksinghasane14@gmail.com>
2019-10-19 23:35:54 +0530

    Product hunt working

--- product_hunt/urls.py
+++ product_hunt/urls.py
@@ -2,10 +2,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from products import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
-    path('accounts/', include('accounts.urls'), name = 'accounts')
-]
+    path('accounts/', include('accounts.urls'), name = 'accounts'),
+    path('products/', include('products.urls'), name = 'products'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def home(request):
-    context = {
-        
-    }
-    return render(request, 'home.html', context)
+    return render(request, 'home.html')
+
+
+
+@login_required
+def create(request):
+    return render(request, 'create.html')
+
+
